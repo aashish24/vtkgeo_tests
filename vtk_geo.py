93e26935d09c4c5bd5b92fa6874116710b7b7a09
Scott Wittenburg <scott.wittenburg@kitware.com>
2018-03-12 14:46:34 -0600

    Add example illustrating cropping a geojson file

--- vtk_geo.py
+++ vtk_geo.py
@@ -48,8 +48,11 @@ def renderPolyData(ren, vtk_data):
     actor.SetMapper(mapper)
     ren.AddActor(actor)
 
+    return actor
+
 def renderImageData(ren, vtkidata):
-    ren.AddActor(vtkImageDataToActor(vtkidata))
+    actor = vtkImageDataToActor(vtkidata)
+    ren.AddActor(actor)
 
 def renderMultiblockData(ren, vtkmdata):
     mapper = vtk.vtkCompositePolyDataMapper()
@@ -58,6 +61,7 @@ def renderMultiblockData(ren, vtkmdata):
     actor = vtk.vtkActor()
     actor.SetMapper(mapper)
     ren.AddActor(actor)
+    return actor
 
 def renderStructureGridData(vtk_data):
     ren = vtk.vtkRenderer()
@@ -81,6 +85,21 @@ def renderStructureGridData(vtk_data):
     iren.Initialize()
     renWin.Render()
     iren.Start()
+
+def loadGeoJSONData(filename):
+    geoString = None
+
+    with open(filename, 'r') as f:
+        geoString = f.read()
+
+    reader = vtk.vtkGeoJSONReader()
+    reader.StringInputModeOn()
+    reader.SetStringInput(geoString)
+
+    reader.Update()
+    dataset = reader.GetOutput()
+
+    return dataset
 
 def loadRasterData(filename):
     reader = vtk.vtkGDALRasterReader()
@@ -172,13 +191,54 @@ def runPointCloudPlusRasterExample(p_filename, r_filename):
     renderImageData(ren, vtkidata)
     run()
 
-runRasterExample("./data/o_14DEC14WV031100014DEC14160402-pans-utm.tif")
+def runGeoJSONCropExample(input_filename, bbox_filename):
+    inputDataset = loadGeoJSONData(input_filename)
+    bboxDataset = loadGeoJSONData(bbox_filename)
+
+    # Use the bounds from the bounding box to create an implicit function
+    queryBounds = list(bboxDataset.GetBounds())
+    bbox = vtk.vtkPlanes()
+    bbox.SetBounds(queryBounds)
+
+    # This filter uses the implicit function to extract the data
+    bboxFilter = vtk.vtkExtractPolyDataGeometry()
+    bboxFilter.SetInputData(inputDataset)
+    bboxFilter.SetImplicitFunction(bbox)
+    # Exclude (set to 0) or include (set to 1) districts on the boundary
+    bboxFilter.SetExtractBoundaryCells(0)
+
+    # Get the cropped region and write it to disk
+    bboxFilter.Update()
+    croppedDataset = bboxFilter.GetOutput()
+    outputWriter = vtk.vtkGeoJSONWriter()
+    outputWriter.SetFileName('cropped_output.geojson')
+    outputWriter.SetInputData(croppedDataset)
+    outputWriter.Write()
+
+    renderer = init()
+    inputActor = renderPolyData(renderer, inputDataset)
+    bboxActor = renderPolyData(renderer, bboxDataset)
+    cropActor = renderPolyData(renderer, croppedDataset)
+
+    # Set rendering type and color on the actors
+    inputActor.GetProperty().SetRepresentationToWireframe()
+    inputActor.GetProperty().SetColor(0.0, 1.0, 0.0)
+    bboxActor.GetProperty().SetRepresentationToWireframe()
+    bboxActor.GetProperty().SetColor(1.0, 0.0, 0.0)
+    cropActor.GetProperty().SetRepresentationToWireframe()
+    cropActor.GetProperty().SetColor(1.0, 0.0, 1.0)
+
+    run()
+
+
+# runRasterExample("./data/o_14DEC14WV031100014DEC14160402-pans-utm.tif")
 # runVectorExample("./data/jacksonville/jacksonville_3d_bldgs_1.shp")
 # runPointCloudExample("./data/tp_manual_20171031104346_flt.bpf")
 # runPointCloudPlusRasterExample("./data/tp_manual_20171031104346_flt.bpf",
 #                                "./data/o_14DEC14WV031100014DEC14160402-P1BS-500648062060_01_P001_________AAE_0AAAAABPABS0_utm.tif")
 
 # Cropping - Scott
+runGeoJSONCropExample("./data/baghdad_districts.geojson", "./data/baghdad_bbox.geojson")
 # Building extraction example - Scott/Dan/Aashish
 # SegY 3D visualization - Aashish/Scott
 
